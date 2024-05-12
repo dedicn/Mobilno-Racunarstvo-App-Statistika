94125d03aaccf4f8b5523875b30fbda9ca65dc26
Nevena Dedic <nevenadedic@gmail.com>
2024-05-12 23:10:55 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'statistics-app',
+  webDir: 'www'
+};
+
+export default config;
